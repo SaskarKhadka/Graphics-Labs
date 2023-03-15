f5fb2cdfbc8c55441d7fc44a590c2e200d75a414
Saskar Khadka <saskar.khadka@gmail.com>
2023-03-16 01:31:52 +0545

    comments

--- Lab 1/flagComponents/moon.js
+++ Lab 1/flagComponents/moon.js
@@ -18,23 +18,27 @@ function createMoon() {
         normalise(160, canvasWidth), normalise(340, canvasHeight), 0.0,
     ] 
 
+    // Create a circle
     createCircle(whiteFragCode, referenceLine)
 
     referenceLine = [normalise(130, canvasWidth), normalise(353, canvasHeight), 0.0,
         normalise(100, canvasWidth), normalise(353, canvasHeight), 0.0,
     ] 
 
+    // Overlap the previous circle to create the crescent
     createCircle(redFragCode, referenceLine)
 
     referenceLine = [normalise(130, canvasWidth), normalise(330, canvasHeight), 0.0,
         normalise(115, canvasWidth), normalise(330, canvasHeight), 0.0,
     ] 
 
+    // Create the rising sun
     createCircle(whiteFragCode, referenceLine, 1, isSemiCircle = false)
     
     referenceLine = [normalise(130, canvasWidth), normalise(330, canvasHeight), 0.0,
         normalise(150, canvasWidth), normalise(330, canvasHeight), 0.0,
     ] 
 
+    // Create the sun rays
     createRays(whiteFragCode, referenceLine, 10)
 }
--- Lab 1/flagComponents/sun.js
+++ Lab 1/flagComponents/sun.js
@@ -7,11 +7,14 @@ function createSun(fragCode) {
     let referenceLine = [normalise(130, canvasWidth), normalise(140, canvasHeight), 0.0,
         normalise(157, canvasWidth), normalise(140, canvasHeight), 0.0,
     ] 
+
+    // Create the sun
     createCircle(fragCode, referenceLine, true)
 
     referenceLine = [normalise(130, canvasWidth), normalise(140, canvasHeight), 0.0,
         normalise(165, canvasWidth), normalise(140, canvasHeight), 0.0,
     ] 
 
+    // Create the sun rays
     createRays(fragCode, referenceLine, 8)
 }
